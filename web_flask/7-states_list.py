7ac1b90925269e33d79c163477f18f7b27d4371c
KagisoMakgaba1 <kagisomakgaba99@gmail.com>
2024-05-19 07:45:52 -0700

    7-states_list.py

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,30 +1,25 @@
 #!/usr/bin/python3
+"""
+start Flask application
+"""
 
-"""This script starts the flask application and runs the web server."""
-
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template
+from models import *
 from models import storage
-from models.state import State
-
 app = Flask(__name__)
-app.jinja_env.trim_blocks = True
-app.jinja_env.lstrip_blocks = True
+
+
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    """display a HTML page with the states listed in alphabetical order"""
+    states = sorted(list(storage.all("State").values()), key=lambda x: x.name)
+    return render_template('7-states_list.html', states=states)
 
 
 @app.teardown_appcontext
-def teardown_session(_):
-    """Closes the current session after each request."""
+def teardown_db(exception):
+    """closes the storage on teardown"""
     storage.close()
 
-
-@app.route("/states_list", strict_slashes=False)
-def get_states():
-    """Renders all the State objects available in the storage."""
-    states = storage.all(State).values()
-
-    return render_template("7-states_list.html", states=states)
-
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
