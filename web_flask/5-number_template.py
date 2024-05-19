60c0948c2a96e69899869eb44c3a0506e1702c7b
KagisoMakgaba1 <kagisomakgaba99@gmail.com>
2024-05-19 07:54:33 -0700

    5-number_template.py

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,50 +1,55 @@
 #!/usr/bin/python3
+"""Start web application with two routings
+"""
 
-"""This module contains routes for the AirBnB web project."""
-
-from markupsafe import escape
-from flask import render_template
-from flask import Flask
-
+from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route("/", strict_slashes=False)
-def home():
-    """Returns a simple string for the homepage."""
-    return "Hello HBNB!"
+@app.route('/')
+def hello():
+    """Return string when route queried
+    """
+    return 'Hello HBNB!'
 
 
-@app.route("/hbnb", strict_slashes=False)
+@app.route('/hbnb')
 def hbnb():
-    """Handles the /hbnb route."""
-    return "HBNB"
+    """Return string when route queried
+    """
+    return 'HBNB'
 
 
-@app.route("/c/<text>", strict_slashes=False)
-def c_route(text):
-    """Returns a note about the C language."""
-    return f"C {escape(text.replace('_', ' '))}"
+@app.route('/c/<text>')
+def c_is_fun(text):
+    """Return reformatted text
+    """
+    return 'C ' + text.replace('_', ' ')
 
 
-@app.route("/python/", strict_slashes=False)
-@app.route("/python/<text>", strict_slashes=False)
-def python_route(text="is cool"):
-    """Returns a note about the Python language."""
-    return f"Python {escape(text.replace('_', ' '))}"
+@app.route('/python/')
+@app.route('/python/<text>')
+def python_with_text(text='is cool'):
+    """Reformat text based on optional variable
+    """
+    return 'Python ' + text.replace('_', ' ')
 
 
-@app.route("/number/<int:n>", strict_slashes=False)
-def number(n):
-    """Returns a string stating an input is a number if it is an integer."""
-    return f"{n} is a number"
+@app.route('/number/<int:n>')
+def number(n=None):
+    """Allow request if path variable is a valid integer
+    """
+    return str(n) + ' is a number'
 
 
-@app.route("/number_template/<int:n>", strict_slashes=False)
+@app.route('/number_template/<int:n>')
 def number_template(n):
-    """Renders information about a number."""
-    return render_template("5-number.html", number=n)
+    """Retrieve template for request
+    """
+    path = '5-number.html'
+    return render_template(path, n=n)
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+if __name__ == '__main__':
+    app.url_map.strict_slashes = False
+    app.run(host='0.0.0.0', port=5000)
